28c852bbcb478190f3d7e7b065b55d015f8852ce
ashrafuldalif <ashrafulalif26@gmail.com>
2023-12-23 21:11:21 +0600

    adding timer page

--- clock/app.js
+++ clock/app.js
@@ -20,16 +20,16 @@ for (var i = 0; i < btns.length; i++) {
 
 // clock____________________
 
-let alltime = document.querySelector(".clock");
-let hour = alltime.querySelector("#hour");
-let min = alltime.querySelector("#min");
-let sec = alltime.querySelector("#sec");
-let day = alltime.querySelector("#day");
-let allday = document.querySelector(".dateBox");
-let wday = allday.querySelector("#weekDay");
-let tarikh = allday.querySelector("#date");
-let links = document.querySelectorAll("nav a");
-let sections = document.querySelectorAll("section");
+const alltime = document.querySelector(".clock");
+const hour = alltime.querySelector("#hour");
+const min = alltime.querySelector("#min");
+const sec = alltime.querySelector("#sec");
+const day = alltime.querySelector("#day");
+const allday = document.querySelector(".dateBox");
+const wday = allday.querySelector("#weekDay");
+const tarikh = allday.querySelector("#date");
+const links = document.querySelectorAll("nav a");
+const sections = document.querySelectorAll("section");
 
 
 // clock _________________________________________
@@ -76,14 +76,14 @@ function checkHour(e) {
 
 // stop watch ______________________________________
 
-let stopWatchThings = document.querySelector("#stopWatchPage");
-let SWmilisec = stopWatchThings.querySelector("#swMiliSec");
-let SWsec = stopWatchThings.querySelector("#swsec");
-let SWmin = stopWatchThings.querySelector("#swmin");
-let SWhour = stopWatchThings.querySelector("#swhour");
-let stopBtn = stopWatchThings.querySelector("#stopBtn");
-let pauseBtn = stopWatchThings.querySelector("#pauseBtn");
-let startBtn = stopWatchThings.querySelector("#startBtn");
+const stopWatchThings = document.querySelector("#stopWatchPage");
+const SWmilisec = stopWatchThings.querySelector("#swMiliSec");
+const SWsec = stopWatchThings.querySelector("#swsec");
+const SWmin = stopWatchThings.querySelector("#swmin");
+const SWhour = stopWatchThings.querySelector("#swhour");
+const stopBtn = stopWatchThings.querySelector("#stopBtn");
+const pauseBtn = stopWatchThings.querySelector("#pauseBtn");
+const startBtn = stopWatchThings.querySelector("#startBtn");
 
 let sMilisec = 0;
 let sSec = 0;
@@ -110,7 +110,6 @@ stopBtn.addEventListener("click",()=>{
   SWhour.innerHTML="00";
 
 })
-
 
 startBtn.addEventListener("click",()=>{
   if(!start || p==true ){
@@ -141,4 +140,80 @@ if (sMilisec >99) {
     }
   }
 }
+}
+// timer part  ______________________________________________________
+
+const timerThings = document.querySelector("#timerPage");
+const timer = document.querySelector(".timer");
+const tInputHour = timerThings.querySelector("#TMRhour");
+const tInputMin = timerThings.querySelector("#TMRmin");
+const tInputsec = timerThings.querySelector("#TMRsec");
+const resetTimer= timerThings.querySelector("#timerReset");
+const submitBtn= timerThings.querySelector("#submitBtn");
+const timerDisplay = timerThings.querySelector(".timerDisplay");
+const timerBox=timerThings.getElementsByClassName('timerBox');
+const timerArrowRapper = document.querySelectorAll(".timerArrowRapper");
+
+let check=true;
+let remainingTimeInSec,remainings;
+timerArrowRapper.forEach((num) => {
+  const numInput = num.querySelector(".timerBox");
+  const arrup = num.querySelector(".up");
+  const arrdown = num.querySelector(".down");
+  arrup.addEventListener("click", () => {
+    numInput.stepUp();
+    checkMaxMin();
+  });
+  arrdown.addEventListener("click", () => {
+    numInput.stepDown();
+    checkMaxMin();
+  });
+});
+function startTimer(){
+  if(check==true ){
+    check=false;
+  let timerHour=parseInt(tInputHour.value,10);
+  let timerMin=parseInt(tInputMin.value,10);
+  let timerSec=parseInt(tInputsec.value,10);
+  let totalTimeInSec=timerHour*3600+timerMin*60+timerSec;
+
+  if (remainingTimeInSec) {
+    totalTimeInSec = remainingTimeInSec;
+    remainingTimeInSec = undefined; 
+  }
+countdown = setInterval(function () {
+  timerDisplay.classList.remove("hide");
+  timer.classList.add("hide");
+  const hours = Math.floor(totalTimeInSec / 3600);
+  const minutes = Math.floor((totalTimeInSec % 3600) / 60);
+  const seconds = totalTimeInSec % 60;
+
+timerDisplay.innerHTML = `${String(hours).padStart(2, "0")} : ${String(minutes).padStart(2, "0")} : ${String(seconds).padStart(2, "0")}`;
+
+  if (totalTimeInSec <= 0) {
+    clearInterval(countdown);
+    timerDisplay.innerHTML = "Time's up!";
+  timesUp();
+  }
+remainings=totalTimeInSec-1;
+  totalTimeInSec--;
+}, 1000);
+}}
+function endTimer() {
+  clearInterval(countdown);
+  tInputHour.value = 0;
+  tInputMin.value = 0;
+  tInputsec.value = 0;
+  timer.classList.remove("hide");
+  timerDisplay.classList.add("hide");
+  check=true;
+}
+function pauseTimer(){
+  clearInterval(countdown);
+remainingTimeInSec=remainings;
+  check=true;
+}
+function timesUp(){
+  timerBox.classList.add("hide");
+  
 }
